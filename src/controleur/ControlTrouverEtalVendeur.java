67ca7c7f75a93bf584078f05605e8d050ba8ece3
JulMig <julie.mignotte8@gmail.com>
2023-02-24 15:33:15 +0100

    Etape 6

--- src/controleur/ControlTrouverEtalVendeur.java
+++ src/controleur/ControlTrouverEtalVendeur.java
@@ -1,5 +1,6 @@
 package controleur;
 
+import personnages.Gaulois;
 import villagegaulois.Etal;
 import villagegaulois.Village;
 
@@ -11,8 +12,11 @@ public class ControlTrouverEtalVendeur {
 	}
 
 	public Etal trouverEtalVendeur(String nomVendeur) {
-		//TODO a completer
 		Etal etal = null;
+		Gaulois habitant = village.trouverHabitant(nomVendeur);
+		if (habitant != null) {
+			etal = village.rechercherEtal(habitant);
+		}
 		return etal;
 	}
 }
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -13,7 +13,31 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			
+			System.out.println("Bonjour " + nomVendeur + " je vais regarder si je peux vous trouver un étal.\n");
+			
+			if (!controlPrendreEtal.resteEtals()) {
+				
+				System.out.println("Désolée " + nomVendeur + " je n'ai plus d'étal qui ne soit déjà occupé.\n");
+				
+			} else {
+				
+				System.out.println("C'est parfait, il me reste un étal pour vous !\n"
+						+ "Il me faudrait quelques renseignements :\n"
+						+ "Quel produit souhaitez-vous vendre ?");
+				String produit = scan.next();
+				int nbProduit = Clavier.entrerEntier("Combien souhaitez-vous en vendre ?");
+			
+				int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
+				
+				if (numeroEtal != -1) {
+					System.out.println("Le vendeur " + nomVendeur + "s'est installé à l'étal n°" + numeroEtal);
+				} 
+			}
+		} else {
+			System.out.println("Je suis désolé " + nomVendeur + " mais il faut être un habitant de notre village pour commercer ici.");
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
